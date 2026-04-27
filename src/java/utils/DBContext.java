8aba0b88d0e99aad529082e7e325b3c12c720daf
Nguyen-Thanh-Dung <tt4089098@gmail.com>
2026-04-27 15:00:48 +0700

    lỗi tùm lum tiếp 2

--- src/java/utils/DBContext.java
+++ src/java/utils/DBContext.java
@@ -1,42 +1,31 @@
 package utils;
 
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Properties;
+import java.util.ResourceBundle;
 
 public class DBContext {
 
     public Connection getConnection() throws Exception {
-        Properties properties = new Properties();
-        
-        // 1. Dùng ClassLoader mạnh nhất của Tomcat
-        InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("database.properties");
-        
-        // 2. Kế hoạch dự phòng: Tìm trong package utils (nếu ông vẫn để file ở chỗ cũ)
-        if (inputStream == null) {
-            inputStream = DBContext.class.getResourceAsStream("/utils/database.properties");
-        }
-        
-        // 3. Kế hoạch dự phòng 2: Tìm trong package resources (nếu ông lỡ tạo package tên là resources)
-        if (inputStream == null) {
-            inputStream = DBContext.class.getResourceAsStream("/resources/database.properties");
-        }
+        try {
+            // Vũ khí tối thượng: Tự động tìm file database.properties trong package utils
+            // Lưu ý: Chỉ viết "utils.database" (không có đuôi .properties)
+            ResourceBundle bundle = ResourceBundle.getBundle("utils.database");
 
-        if (inputStream == null) {
-            throw new Exception("Lỗi tận mạng: Đã tìm ở thu muc goc, thu muc utils, thu muc resources ma van KHONG THAY file database.properties!");
+            String dbUrl = bundle.getString("DB_URL");
+            String user = bundle.getString("DB_USER");
+            String pass = bundle.getString("DB_PASSWORD");
+
+            String params = "?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Ho_Chi_Minh&useSSL=false&allowPublicKeyRetrieval=true";
+            String finalUrl = dbUrl + params;
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(finalUrl, user, pass);
+            
+        } catch (Exception e) {
+            System.err.println("=== LỖI KINH HOÀNG: KHÔNG ĐỌC ĐƯỢC FILE ===");
+            e.printStackTrace();
+            throw new Exception("Quá mệt mỏi! Lỗi thật sự là: " + e.getMessage());
         }
-        
-        properties.load(inputStream);
-
-        String dbUrl = properties.getProperty("DB_URL");
-        String user = properties.getProperty("DB_USER");
-        String pass = properties.getProperty("DB_PASSWORD");
-
-        String params = "?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Ho_Chi_Minh&useSSL=false&allowPublicKeyRetrieval=true";
-        String finalUrl = dbUrl + params;
-        
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(finalUrl, user, pass);
     }
 }
