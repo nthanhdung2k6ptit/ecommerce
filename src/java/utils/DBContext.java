312102c8dc25dc639bab7b2e65870938a0bf3c31
Nguyen-Thanh-Dung <tt4089098@gmail.com>
2026-05-01 09:07:28 +0700

    update file DBContext

--- src/java/utils/DBContext.java
+++ src/java/utils/DBContext.java
@@ -7,25 +7,32 @@ import java.util.ResourceBundle;
 public class DBContext {
 
     public Connection getConnection() throws Exception {
+        // Chỉ gọi "utils.database" - ResourceBundle tự động hiểu là đi tìm file database.properties trong package utils
+        ResourceBundle bundle = ResourceBundle.getBundle("utils.database");
+
+        String dbUrl = bundle.getString("DB_URL");
+        String user = bundle.getString("DB_USER");
+        String pass = bundle.getString("DB_PASSWORD");
+
+        String params = "?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Ho_Chi_Minh&useSSL=false&allowPublicKeyRetrieval=true";
+        String finalUrl = dbUrl + params;
+        
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(finalUrl, user, pass);
+    }
+
+    // Hàm main để test thử luôn không cần bật web
+    public static void main(String[] args) {
         try {
-            // Vũ khí tối thượng: Tự động tìm file database.properties trong package utils
-            // Lưu ý: Chỉ viết "utils.database" (không có đuôi .properties)
-            ResourceBundle bundle = ResourceBundle.getBundle("utils.database");
-
-            String dbUrl = bundle.getString("DB_URL");
-            String user = bundle.getString("DB_USER");
-            String pass = bundle.getString("DB_PASSWORD");
-
-            String params = "?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Ho_Chi_Minh&useSSL=false&allowPublicKeyRetrieval=true";
-            String finalUrl = dbUrl + params;
-            
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(finalUrl, user, pass);
-            
-        } catch (Exception e) {
-            System.err.println("=== LỖI KINH HOÀNG: KHÔNG ĐỌC ĐƯỢC FILE ===");
-            e.printStackTrace();
-            throw new Exception("Quá mệt mỏi! Lỗi thật sự là: " + e.getMessage());
+            DBContext db = new DBContext();
+            Connection conn = db.getConnection();
+            if (conn != null) {
+                System.out.println(">>> ĐÃ THÔNG NÒNG DATABASE! MỌI THỨ CHẠY NGON!");
+                conn.close();
+            }
+        } catch (Exception ex) {
+            System.err.println(">>> VẪN TẠCH!");
+            ex.printStackTrace();
         }
     }
 }
